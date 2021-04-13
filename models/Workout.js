ad4adf1a95cdbd690aaa9662610abaeca58b78cd
HelenLindberg <helenilindberg@yahoo.com>
2021-04-13 11:21:10 -0500

    api routes reworked, exercise model deleted, server file updated

--- models/Workout.js
+++ models/Workout.js
@@ -2,18 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    day: { type: Date, required: true },
+const WorkoutSchema = new Schema({
+    day: { type: Date, required: true, default: Date.now },
     exercises: [
         {
             type: {
-                type: String
+                type: String,
+                trim: true,
+                required: true
             },
             name: {
-                type: String
+                type: String,
+                trim: true,
+                required: true
             },
             duration: {
-                type: Number
+                type: Number,
+                required: true
             },
             weight: {
                 type: Number
@@ -23,11 +28,14 @@ const workoutSchema = new Schema({
             },
             sets: {
                 type: Number
+            },
+            distance: {
+                type: Number
             }
         }
     ]
 });
 
-const workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("workout", WorkoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 const express = require("express");
-const path = require("path");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const apiRoutes = require("./routes/api");
-const viewRoutes = require("./routes/views");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(logger("dev"));
+
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
       useFindAndModify: false
     }
   );
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static("public"));
+  require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
 
-pp.use(apiRoutes);
-app.use(viewRoutes);
-
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
   });
-  
-  app.listen(PORT, () => console.log("listening on port: ", PORT));
