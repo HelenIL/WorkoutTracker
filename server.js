14c68f5837288452fa372bd4265ae57091549f1c
HelenLindberg <helenilindberg@yahoo.com>
2021-04-13 11:52:46 -0500

    server file bug fixed, app working

--- server.js
+++ server.js
@@ -3,20 +3,25 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
       useNewUrlParser: true,
-      useFindAndModify: false
+      useFindAndModify: false,
+      useUnifiedTopology: true 
     }
   );
 
-  require("./routes/htmlroutes")(app);
-require("./routes/apiroutes")(app);
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
