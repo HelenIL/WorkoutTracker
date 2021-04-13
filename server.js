3d78a165c22026a0bbc3027c770f4c6f57d62ab1
HelenLindberg <helenilindberg@yahoo.com>
2021-04-13 10:36:24 -0500

    server file created, mongo atlas and heroku connection made

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const apiRoutes = require("./routes/api");
+const viewRoutes = require("./routes/views");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+  app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+pp.use(apiRoutes);
+app.use(viewRoutes);
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+  
+  app.listen(PORT, () => console.log("listening on port: ", PORT));
